22f6cce64aecde54b90b46060df2a8673846e4ba
joanterm <jo_joanarg@yahoo.com>
2021-11-15 12:24:14 -0500

    Added blog list component and routed pages

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList  = (props) => {
     const blogs = props.blogs 
     const title = props.title
@@ -11,9 +13,10 @@ const BlogList  = (props) => {
             <h2>{title}</h2>
                     {blogs.map((element) => (
                 <div className="blog-preview" key={element.id}>
-                    <h2>{element.title}</h2>
-                    <p>Written by: {element.author}</p>
-    
+                    <Link to={`/blogs/${element.id}`}>
+                        <h2>{element.title}</h2>
+                        <p>Written by: {element.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -17,24 +17,33 @@ const Home = () => {
     // }
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogsss")
+        //AbortController will prevent fetching in the background/errors when we switch to a different page
+        const abortCont = new AbortController()
+
+        fetch("http://localhost:8000/blogs", {signal: abortCont.signal}) //part of abort error
         .then((response) => {
             if (!response.ok) {
-                throw new Error("Could not fetch data")
+                throw Error(`Error getting the data. Your error code is: ${response.status}`)
             }
             return response.json()
         })
         .then((data) => {
             console.log(data)
-            setBlogs(data)
+            setBlogs(data) 
             setIsLoading(false) //once data gets fetched, it will remove loading massage
-        })
-        .catch((error) => {
-            console.log(error)
-            setError(error)
-            setIsLoading(false)
             setError(null)
         })
+        .catch((err) => {
+            if (err.name === "AbortError") { //it has to be equal to "AbortError"
+            } else {
+                console.log(err.message)
+                setError(err.message)
+                setIsLoading(false)
+            }
+        })
+        return () => {
+            abortCont.abort() //part of abort error
+        }
     }, []);
 
     return ( 
@@ -49,6 +58,11 @@ const Home = () => {
         </div>
      );
 }
- 
+
 export default Home;
 
+// TO RUN A SERVER:
+// npx json-server --watch data/db.json --port 8000
+
+// API ENDPOINT FROM RESOURCES IN THE TERMINAL: 
+// http://localhost:8000/blogs
